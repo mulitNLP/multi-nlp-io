c4b4f66c7cd115f0bebb903253811eebb8ceb5d1
bknote71 <bknote71@gmail.com>
2023-07-23 20:57:52 +0900

    local script

--- startapplocal.sh
+++ startapplocal.sh
@@ -1,12 +1,11 @@
 RESOURCES_PATH="./src/main/resources"
 TARGET_PORT=8080
 
-npm install
+# npm install
 npm run build
 
 # copy
 cp -f  "${RESOURCES_PATH}/static/dist/index.html" "${RESOURCES_PATH}/templates/"
-# echo "시발" > /home/ec2-user/nohup.out
 
 process_id=$(lsof -ti:8080)
 
@@ -15,4 +14,4 @@ if [ -n "$process_id" ]; then
 fi
 
 ./gradlew clean build
-nohup java -jar -Dserver.port=${TARGET_PORT} /home/ec2-user/multi-nlpgame/build/libs/* > /home/ec2-user/nohup.out 2>&1 &
+java -jar /home/ec2-user/multi-nlpgame/build/libs/*
