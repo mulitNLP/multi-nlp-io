06b37de64b2958694739e87eb8adcfbd8cbc38da
bknote71 <bknote71@gmail.com>
2023-07-26 21:40:54 +0900

    networking localhost

--- startdocker.sh
+++ startdocker.sh
@@ -9,15 +9,15 @@ if [ "$(docker ps -q -f name=boot_app)" ]; then
     docker rm boot_app
 fi
 
-if [ "$(docker ps -q -f name=nlp_app)" ]; then
-    # 컨테이너가 실행 중이면 중지(stop)하고 삭제(remove)
-    docker stop nlp_app
-    docker rm nlp_app
-fi
+#if [ "$(docker ps -q -f name=nlp_app)" ]; then
+#    # 컨테이너가 실행 중이면 중지(stop)하고 삭제(remove)
+#    docker stop nlp_app
+#    docker rm nlp_app
+#fi
 
 # 도커 기동
 cd ${WORKDIRECTORY}
 docker build -t boot_app ${WORKDIRECTORY}
-docker build -t nlp_app "${WORKDIRECTORY}/nlp-check"
+#docker build -t nlp_app "${WORKDIRECTORY}/nlp-check"
 
 docker-compose --compatibility up -d
