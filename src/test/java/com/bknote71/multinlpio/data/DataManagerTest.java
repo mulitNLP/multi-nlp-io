7c56cfeac56f3891c068a602972a00db857cf236
bknote71 <bknote71@gmail.com>
2023-07-26 21:34:25 +0900

    fix bug

--- src/test/java/com/bknote71/multinlpio/data/DataManagerTest.java
+++ src/test/java/com/bknote71/multinlpio/data/DataManagerTest.java
@@ -18,30 +18,30 @@ class DataManagerTest {
 
     @Test
     void word() {
-        DataManager.loadWord();
+//        DataManager.loadWord();
 //        for (WordData word : DataManager.words) {
 //            System.out.println(word.getWord() + " " + word.getType());
 //        }
 
-        long 명_count = Arrays.stream(DataManager.words)
-                .filter(w -> w.getType().equals("명"))
-                .count();
-
-        long 형_count = Arrays.stream(DataManager.words)
-                .filter(w -> w.getType().equals("형"))
-                .count();
-
-        long 부_count = Arrays.stream(DataManager.words)
-                .filter(w -> w.getType().equals("부"))
-                .count();
-
-        long 동_count = Arrays.stream(DataManager.words)
-                .filter(w -> w.getType().equals("동"))
-                .count();
-
-        System.out.println(명_count);
-        System.out.println(형_count);
-        System.out.println(부_count);
-        System.out.println(동_count);
+//        long 명_count = Arrays.stream(DataManager.words)
+//                .filter(w -> w.getType().equals("명"))
+//                .count();
+//
+//        long 형_count = Arrays.stream(DataManager.words)
+//                .filter(w -> w.getType().equals("형"))
+//                .count();
+//
+//        long 부_count = Arrays.stream(DataManager.words)
+//                .filter(w -> w.getType().equals("부"))
+//                .count();
+//
+//        long 동_count = Arrays.stream(DataManager.words)
+//                .filter(w -> w.getType().equals("동"))
+//                .count();
+//
+//        System.out.println(명_count);
+//        System.out.println(형_count);
+//        System.out.println(부_count);
+//        System.out.println(동_count);
     }
 }
